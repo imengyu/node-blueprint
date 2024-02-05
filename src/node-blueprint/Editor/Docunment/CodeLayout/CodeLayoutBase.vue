c0d5fc6d619092f622939262a4c1669917ff44cb
imengyu <1501076885@qq.com>
2024-02-05 16:46:44 +0800

    primarySideBar位置切换

--- src/node-blueprint/Editor/Docunment/CodeLayout/CodeLayoutBase.vue
+++ src/node-blueprint/Editor/Docunment/CodeLayout/CodeLayoutBase.vue
@@ -50,43 +50,78 @@
             |    | panel   |     |
             +----+---------+-----+
           -->
-          <SplitThird 
-            v-if="config.bottomAlignment === 'center' || config.bottomAlignment === 'justify'"
-            v-model:sizeLeft="config.primarySideBarWidth"
-            v-model:sizeRight="config.secondarySideBarWidth"
-            :showLeft="primarySideBar"
-            :showRight="secondarySideBar"
-            :leftMinSize="config.primarySideBarMinWidth"
-            :rightMinSize="config.secondarySideBarWidth"
-            @closeLeft="(v) => $emit('update:primarySideBar', v)"
-            @closeRight="(v) => $emit('update:secondarySideBar', v)"
-          >
-            <template #left>
-              <slot v-if="config.primarySideBarPosition === 'left'" name="primarySideBar" />
-              <slot v-else name="secondarySideBar" />
-            </template>
-            <template #center>
-              <SplitBase
-                :size="100 - config.bottomPanelHeight"
-                :horizontal="false"
-                :showSecond="bottomPanel && config.bottomAlignment === 'center'"
-                :secondMinSize="config.bottomPanelMinHeight"
-                @update:size="(s) => config.bottomPanelHeight = 100 - s"
-                @closeSecond="(v) => $emit('update:bottomPanel', v)"
-              >
-                <template #first>
-                  <slot name="centerArea" />
-                </template>
-                <template #second>
-                  <slot name="bottomPanel" />
-                </template>
-              </SplitBase>
-            </template>
-            <template #right>
-              <slot v-if="config.primarySideBarPosition === 'right'" name="primarySideBar" />
-              <slot v-else name="secondarySideBar" />
-            </template>
-          </SplitThird>
+          <template v-if="config.bottomAlignment === 'center' || config.bottomAlignment === 'justify'">
+            <SplitThird 
+              v-if="config.primarySideBarPosition === 'left'"
+              v-model:sizeLeft="config.primarySideBarWidth"
+              v-model:sizeRight="config.secondarySideBarWidth"
+              :showLeft="primarySideBar"
+              :showRight="secondarySideBar"
+              :leftMinSize="config.primarySideBarMinWidth"
+              :rightMinSize="config.secondarySideBarWidth"
+              @closeLeft="(v) => $emit('update:primarySideBar', v)"
+              @closeRight="(v) => $emit('update:secondarySideBar', v)"
+            >
+              <template #left>
+                <slot name="primarySideBar" />
+              </template>
+              <template #center>
+                <SplitBase
+                  :size="100 - config.bottomPanelHeight"
+                  :horizontal="false"
+                  :showSecond="bottomPanel && config.bottomAlignment === 'center'"
+                  :secondMinSize="config.bottomPanelMinHeight"
+                  @update:size="(s) => config.bottomPanelHeight = 100 - s"
+                  @closeSecond="(v) => $emit('update:bottomPanel', v)"
+                >
+                  <template #first>
+                    <slot name="centerArea" />
+                  </template>
+                  <template #second>
+                    <slot name="bottomPanel" />
+                  </template>
+                </SplitBase>
+              </template>
+              <template #right>
+                <slot name="secondarySideBar" />
+              </template>
+            </SplitThird>
+            <SplitThird 
+              v-if="config.primarySideBarPosition === 'right'"
+              v-model:sizeLeft="config.secondarySideBarWidth"
+              v-model:sizeRight="config.primarySideBarWidth"
+              :showLeft="secondarySideBar"
+              :showRight="primarySideBar"
+              :leftMinSize="config.secondarySideBarWidth"
+              :rightMinSize="config.primarySideBarMinWidth"
+              @closeLeft="(v) => $emit('update:secondarySideBar', v)"
+              @closeRight="(v) => $emit('update:primarySideBar', v)"
+            >
+              <template #left>
+                <slot name="secondarySideBar" />
+              </template>
+              <template #center>
+                <SplitBase
+                  :size="100 - config.bottomPanelHeight"
+                  :horizontal="false"
+                  :showSecond="bottomPanel && config.bottomAlignment === 'center'"
+                  :secondMinSize="config.bottomPanelMinHeight"
+                  @update:size="(s) => config.bottomPanelHeight = 100 - s"
+                  @closeSecond="(v) => $emit('update:bottomPanel', v)"
+                >
+                  <template #first>
+                    <slot name="centerArea" />
+                  </template>
+                  <template #second>
+                    <slot name="bottomPanel" />
+                  </template>
+                </SplitBase>
+              </template>
+              <template #right>
+                <slot name="primarySideBar" />
+              </template>
+            </SplitThird>
+          </template>
           <!--
             split three grid but bottom panel in left
           
@@ -96,48 +131,89 @@
             |   panel      |     |
             +--------------+-----+
           -->
-          <SplitBase
-            v-if="config.bottomAlignment === 'left'"
-            :size="100 - config.secondarySideBarWidth"
-            :showSecond="secondarySideBar"
-            :secondMinSize="config.secondarySideBarMinWidth"
-            @closeSecond="(v) => $emit('update:secondarySideBar', v)"
-            @update:size="(s) => config.secondarySideBarWidth = 100 - s"
-          >
-            <template #first>
-              <SplitBase
-                v-model:size="config.bottomPanelHeight"
-                :horizontal="false"
-                :showSecond="bottomPanel"
-                :secondMinSize="config.bottomPanelMinHeight"
-                @closeSecond="(v) => $emit('update:bottomPanel', v)"
-              >
-                <template #first>
-                  <SplitBase
-                    v-model:size="config.primarySideBarWidth"
-                    :showFirst="primarySideBar"
-                    :firstMinSize="config.primarySideBarMinWidth"
-                    @closeFirst="(v) => $emit('update:primarySideBar', v)"
-                  >
-                    <template #first>
-                      <slot v-if="config.primarySideBarPosition === 'left'" name="primarySideBar" />
-                      <slot v-else name="secondarySideBar" />
-                    </template>
-                    <template #second>
-                      <slot name="centerArea" />
-                    </template>
-                  </SplitBase>
-                </template>
-                <template #second>
-                  <slot name="bottomPanel" />
-                </template>
-              </SplitBase>
-            </template>
-            <template #second>
-              <slot v-if="config.primarySideBarPosition === 'right'" name="primarySideBar" />
-              <slot v-else name="secondarySideBar" />
-            </template>
-          </SplitBase>
+          <template v-if="config.bottomAlignment === 'left'">
+            <SplitBase
+              v-if="config.primarySideBarPosition === 'left'"
+              :size="100 - config.secondarySideBarWidth"
+              :showSecond="secondarySideBar"
+              :secondMinSize="config.secondarySideBarMinWidth"
+              @closeSecond="(v) => $emit('update:secondarySideBar', v)"
+              @update:size="(s) => config.secondarySideBarWidth = 100 - s"
+            >
+              <template #first>
+                <SplitBase
+                  v-model:size="config.bottomPanelHeight"
+                  :horizontal="false"
+                  :showSecond="bottomPanel"
+                  :secondMinSize="config.bottomPanelMinHeight"
+                  @closeSecond="(v) => $emit('update:bottomPanel', v)"
+                >
+                  <template #first>
+                    <SplitBase
+                      v-model:size="config.primarySideBarWidth"
+                      :showFirst="primarySideBar"
+                      :firstMinSize="config.primarySideBarMinWidth"
+                      @closeFirst="(v) => $emit('update:primarySideBar', v)"
+                    >
+                      <template #first>
+                        <slot v-if="config.primarySideBarPosition === 'left'" name="primarySideBar" />
+                        <slot v-else name="secondarySideBar" />
+                      </template>
+                      <template #second>
+                        <slot name="centerArea" />
+                      </template>
+                    </SplitBase>
+                  </template>
+                  <template #second>
+                    <slot name="bottomPanel" />
+                  </template>
+                </SplitBase>
+              </template>
+              <template #second>
+                <slot name="secondarySideBar" />
+              </template>
+            </SplitBase>
+            <SplitBase
+              v-if="config.primarySideBarPosition === 'right'"
+              :size="100 - config.primarySideBarWidth"
+              :showSecond="primarySideBar"
+              :secondMinSize="config.primarySideBarMinWidth"
+              @closeSecond="(v) => $emit('update:primarySideBar', v)"
+              @update:size="(s) => config.primarySideBarWidth = 100 - s"
+            >
+              <template #first>
+                <SplitBase
+                  v-model:size="config.bottomPanelHeight"
+                  :horizontal="false"
+                  :showSecond="bottomPanel"
+                  :secondMinSize="config.bottomPanelMinHeight"
+                  @closeSecond="(v) => $emit('update:bottomPanel', v)"
+                >
+                  <template #first>
+                    <SplitBase
+                      v-model:size="config.secondarySideBarWidth"
+                      :showFirst="secondarySideBar"
+                      :firstMinSize="config.secondarySideBarMinWidth"
+                      @closeFirst="(v) => $emit('update:secondarySideBar', v)"
+                    >
+                      <template #first>
+                        <slot name="secondarySideBar" />
+                      </template>
+                      <template #second>
+                        <slot name="centerArea" />
+                      </template>
+                    </SplitBase>
+                  </template>
+                  <template #second>
+                    <slot name="bottomPanel" />
+                  </template>
+                </SplitBase>
+              </template>
+              <template #second>
+                <slot name="primarySideBar" />
+              </template>
+            </SplitBase>
+          </template>
           <!--
             split three grid but bottom panel in right 
             
@@ -147,48 +223,90 @@
             |    |     panel     |
             +----+---------------+
           -->
-          <SplitBase
-            v-if="config.bottomAlignment === 'right'"
-            v-model:size="config.primarySideBarWidth"
-            :showFirst="primarySideBar"
-            :firstMinSize="config.primarySideBarMinWidth"
-            @closeFirst="(v) => $emit('update:primarySideBar', v)"
-          >
-            <template #first>
-              <slot v-if="config.primarySideBarPosition === 'left'" name="primarySideBar" />
-              <slot v-else name="secondarySideBar" />
-            </template>
-            <template #second>
-              <SplitBase
-                :size="100 - config.bottomPanelHeight"
-                :horizontal="false"
-                :showSecond="bottomPanel"
-                :secondMinSize="config.bottomPanelMinHeight"
-                @update:size="(v) => config.bottomPanelHeight = 100 - v"
-                @closeSecond="(v) => $emit('update:bottomPanel', v)"
-              >
-                <template #first>
-                  <SplitBase
-                    v-model:size="config.primarySideBarWidth"
-                    :showFirst="primarySideBar"
-                    :firstMinSize="config.primarySideBarMinWidth"
-                    @closeFirst="(v) => $emit('update:primarySideBar', v)"
-                  >
-                    <template #first>
-                      <slot name="centerArea" />
-                    </template>
-                    <template #second>
-                      <slot v-if="config.primarySideBarPosition === 'right'" name="primarySideBar" />
-                      <slot v-else name="secondarySideBar" />
-                    </template>
-                  </SplitBase>
-                </template>
-                <template #second>
-                  <slot name="bottomPanel" />
-                </template>
-              </SplitBase>
-            </template>
-          </SplitBase>
+          <template v-if="config.bottomAlignment === 'right'">
+            <SplitBase
+              v-if="config.primarySideBarPosition === 'left'"
+              v-model:size="config.primarySideBarWidth"
+              :showFirst="primarySideBar"
+              :firstMinSize="config.primarySideBarMinWidth"
+              @closeFirst="(v) => $emit('update:primarySideBar', v)"
+            >
+              <template #first>
+                <slot name="primarySideBar" />
+              </template>
+              <template #second>
+                <SplitBase
+                  :size="100 - config.bottomPanelHeight"
+                  :horizontal="false"
+                  :showSecond="bottomPanel"
+                  :secondMinSize="config.bottomPanelMinHeight"
+                  @update:size="(v) => config.bottomPanelHeight = 100 - v"
+                  @closeSecond="(v) => $emit('update:bottomPanel', v)"
+                >
+                  <template #first>
+                    <SplitBase
+                      :size="100 - config.secondarySideBarWidth"
+                      :showFirst="secondarySideBar"
+                      :secondMinSize="config.secondarySideBarMinWidth"
+                      @update:size="(v) => config.secondarySideBarWidth = 100 - v"
+                      @closeFirst="(v) => $emit('update:secondarySideBar', v)"
+                    >
+                      <template #first>
+                        <slot name="centerArea" />
+                      </template>
+                      <template #second>
+                        <slot name="secondarySideBar" />
+                      </template>
+                    </SplitBase>
+                  </template>
+                  <template #second>
+                    <slot name="bottomPanel" />
+                  </template>
+                </SplitBase>
+              </template>
+            </SplitBase>
+            <SplitBase
+              v-if="config.primarySideBarPosition === 'right'"
+              v-model:size="config.secondarySideBarWidth"
+              :showFirst="secondarySideBar"
+              :firstMinSize="config.secondarySideBarMinWidth"
+              @closeFirst="(v) => $emit('update:secondarySideBar', v)"
+            >
+              <template #first>
+                <slot name="secondarySideBar" />
+              </template>
+              <template #second>
+                <SplitBase
+                  :size="100 - config.bottomPanelHeight"
+                  :horizontal="false"
+                  :showSecond="bottomPanel"
+                  :secondMinSize="config.bottomPanelMinHeight"
+                  @update:size="(v) => config.bottomPanelHeight = 100 - v"
+                  @closeSecond="(v) => $emit('update:bottomPanel', v)"
+                >
+                  <template #first>
+                    <SplitBase
+                      :size="100 - config.primarySideBarWidth"
+                      :showFirst="primarySideBar"
+                      :secondMinSize="config.primarySideBarMinWidth"
+                      @update:size="(v) => config.primarySideBarWidth = 100 - v"
+                      @closeFirst="(v) => $emit('update:primarySideBar', v)"
+                    >
+                      <template #first>
+                        <slot name="centerArea" />
+                      </template>
+                      <template #second>
+                        <slot name="primarySideBar" />
+                      </template>
+                    </SplitBase>
+                  </template>
+                  <template #second>
+                    <slot name="bottomPanel" />
+                  </template>
+                </SplitBase>
+              </template>
+            </SplitBase>
+          </template>
         </template>
         <template #second>
           <slot name="bottomPanel" />
