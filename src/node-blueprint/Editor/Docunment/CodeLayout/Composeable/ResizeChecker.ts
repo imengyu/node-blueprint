ccc8c73f2e9f0d95f6754b83a261d33783610375
快乐的梦鱼 <32213395+imengyu@users.noreply.github.com>
2024-03-21 11:23:02 +0800

    ResizeChecker多定时器优化

--- src/node-blueprint/Editor/Docunment/CodeLayout/Composeable/ResizeChecker.ts
+++ src/node-blueprint/Editor/Docunment/CodeLayout/Composeable/ResizeChecker.ts
@@ -1,5 +1,12 @@
+import ArrayUtils from "@/node-blueprint/Base/Utils/ArrayUtils";
+import { SimpleTimer } from "@/node-blueprint/Base/Utils/Timer/Timer";
 import type { Ref } from "vue";
 
+const globalResizeCheckerList = [] as (() => void)[];
+const globalResizeCheckerTimer = new SimpleTimer(globalResizeCheckerList, () => {
+  for (const cb of globalResizeCheckerList)
+    cb();
+}, 100);
 
 export function useResizeChecker(
   ref: Ref<HTMLElement|undefined>,
@@ -8,32 +15,31 @@ export function useResizeChecker(
   onSizeChange?: (newWidth: number, newHeight: number) => void,
 ) {
 
-  let sizeChangeTimer = 0;
   let sizeChangeLastWidth = 0;
   let sizeChangeLastHeight = 0;
 
+  function checkerCallback() {
+    if (ref.value) {
+      if (onWidthChange && sizeChangeLastWidth !== ref.value.offsetWidth)
+        onWidthChange(ref.value.offsetWidth);
+      if (onHeightChange && sizeChangeLastHeight !== ref.value.offsetHeight)
+        onHeightChange(ref.value.offsetHeight);
+      if (onSizeChange && (sizeChangeLastWidth !== ref.value.offsetWidth || sizeChangeLastHeight !== ref.value.offsetHeight))
+        onSizeChange(ref.value.offsetWidth, ref.value.offsetHeight);
+      sizeChangeLastWidth = ref.value.offsetWidth;
+      sizeChangeLastHeight = ref.value.offsetHeight;
+    }
+  }
+
   return {
     startResizeChecker() {
-      if (sizeChangeTimer > 0)
-        clearInterval(sizeChangeTimer);
-      sizeChangeTimer = setInterval(() => {
-        if (ref.value) {
-          if (onWidthChange && sizeChangeLastWidth !== ref.value.offsetWidth)
-            onWidthChange(ref.value.offsetWidth);
-          if (onHeightChange && sizeChangeLastHeight !== ref.value.offsetHeight)
-            onHeightChange(ref.value.offsetHeight);
-          if (onSizeChange && (sizeChangeLastWidth !== ref.value.offsetWidth || sizeChangeLastHeight !== ref.value.offsetHeight))
-            onSizeChange(ref.value.offsetWidth, ref.value.offsetHeight);
-          sizeChangeLastWidth = ref.value.offsetWidth;
-          sizeChangeLastHeight = ref.value.offsetHeight;
-        }
-      }, 200);
+      globalResizeCheckerTimer.start();
+      globalResizeCheckerList.push(checkerCallback);
     },
     stopResizeChecker() {
-      if (sizeChangeTimer > 0) {
-        clearInterval(sizeChangeTimer);
-        sizeChangeTimer = 0;
-      }
+      ArrayUtils.remove(globalResizeCheckerList, checkerCallback);
+      if (globalResizeCheckerList.length === 0)
+        globalResizeCheckerTimer.stop();
     },
   }
 }
